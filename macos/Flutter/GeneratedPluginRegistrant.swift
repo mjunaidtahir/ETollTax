f247e2123e2ba3638739787cb2eef92d226feeed
Muhammad Junaid Tahir <56703471+mjunaidtahir@users.noreply.github.com>
2024-04-23 06:43:37 +0500

    remove unused packages

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,16 +5,6 @@
 import FlutterMacOS
 import Foundation
 
-import file_selector_macos
-import mobile_scanner
-import path_provider_foundation
-import smart_auth
-import sqflite
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
-  MobileScannerPlugin.register(with: registry.registrar(forPlugin: "MobileScannerPlugin"))
-  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
-  SmartAuthPlugin.register(with: registry.registrar(forPlugin: "SmartAuthPlugin"))
-  SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
 }
